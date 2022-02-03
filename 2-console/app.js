1d2823d17ebea8b3a936d2f8a37e9dbb852d7a42
김지웅 <godjw96@gmail.com>
2022-02-04 05:44:02 +0900

    Create app.js

---
+++ 2-console/app.js
@@ -0,0 +1,12 @@
+console.log("logging...");
+console.clear();
+
+// log level
+console.log("log");
+console.info("info");
+console.warn("warn");
+console.error("error");
+
+// assert
+console.assert(2 === 3, "not same");
+console.assert(2 === 2, "same!");
