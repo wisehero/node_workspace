695512b3b415a6a648c6a06e53e5cfafd5aa3e2c
김지웅 <godjw96@gmail.com>
2022-02-19 01:07:07 +0900

    Update app.js

--- express/app.js
+++ express/app.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res, next) => {
-  console.log("get");
+  console.log(req.path);
+  console.log(req.headers);
+  console.log(req.params);
+  console.log(req.query);
+  res.send("hi");
 });
 app.listen(8080);
